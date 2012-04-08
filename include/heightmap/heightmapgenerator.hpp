df1574588e17d69718edc15ca9b63d515c272f0c
Michel Bouwmans <m.bouwmans@ep-games.eu>
2012-04-08 13:47:03 +0200

    New heightmap-generator which generates vertices

--- include/heightmap/heightmapgenerator.hpp
+++ include/heightmap/heightmapgenerator.hpp
@@ -11,22 +11,30 @@
 typedef std::tr1::shared_ptr<std::vector<int> > IntVectorPtr;
 typedef std::vector<IntVectorPtr > Matrix;
 
+struct HMVertex {
+	int x, y, z;
+};
+
 class HeightmapGenerator {
-public:
-	HeightmapGenerator(int size, int seed, int variation);
-	virtual ~HeightmapGenerator();
+	public:
+		HeightmapGenerator(int size, int seed, int variation);
+		virtual ~HeightmapGenerator();
 
-	void fillMap();
-	const Matrix& getMap();
-private:
-	Matrix map;
-	int variation;
+		void fillMap();
+		const Matrix& getMap();
+		void convertMap();
+		HMVertex* getVertices();
+	private:
+		Matrix map;
+		int mapWidth;
+		int variation;
+		HMVertex* vertices;
 
-	void fillMap(int iteration);
-	void makeDiamond(int, int, int, int, int);
-	void makeSquare(int, int, int, int, int);
-	int randomNumber(int iteration);
-	int numberOfTrailingZeros(int);
+		void fillMap(int iteration);
+		void makeDiamond(int, int, int, int, int);
+		void makeSquare(int, int, int, int, int);
+		int randomNumber(int iteration);
+		int numberOfTrailingZeros(int);
 };
 
 #endif
--- src/heightmap/heightmapgenerator.cpp
+++ src/heightmap/heightmapgenerator.cpp
@@ -1,8 +1,11 @@
-
 #include "heightmap/heightmapgenerator.hpp"
 
+#include <ctime>
+
 HeightmapGenerator::HeightmapGenerator(int size, int seed, int variation) {
+	mapWidth = size;
 	HeightmapGenerator::variation = variation;
+	vertices = new HMVertex[size*size];
 
 	for(int i = 0; i < size; ++i) {
 		IntVectorPtr intVectorPtr(new std::vector<int>(size));
@@ -22,7 +25,11 @@ const Matrix& HeightmapGenerator::getMap() {
 	return map;
 }
 
-void HeightmapGenerator::fillMap(){
+HMVertex* HeightmapGenerator::getVertices() {
+	return vertices;
+}
+
+void HeightmapGenerator::fillMap() {
 	srand(std::time(0));
 	int twoLog = numberOfTrailingZeros(map.size()-1);
 	for(int i = 0; i < (twoLog - 1); i++) {
@@ -30,26 +37,78 @@ void HeightmapGenerator::fillMap(){
 	}
 }
 
+void HeightmapGenerator::convertMap() {
+	int numVertex = 0;
+	int x = 0;
+	int z = 0;
+	int xPrev = -1;
+	int zPrev = -1;
+	for(int mod = 0; mod < mapWidth-1; mod++) {
+		for(x = 0; x < mapWidth; x++) {
+			if(x != xPrev || z != zPrev) {
+				z = mod;
+				HMVertex nextVertex;
+				nextVertex.x = x;
+				nextVertex.z = z;
+				nextVertex.y = (*map[x])[z];
+				vertices[numVertex] = nextVertex;
+				numVertex++;
+			}
+			z = mod+1;
+			HMVertex nextVertex;
+			nextVertex.x = x;
+			nextVertex.z = z;
+			nextVertex.y = (*map[x])[z];
+			vertices[numVertex] = nextVertex;
+			numVertex++;
+			xPrev = x;
+			zPrev = z;
+		}
+		mod++;
+		for(x = mapWidth-1; x >= 0; x--) {
+			if(x != xPrev || z != zPrev) {
+				z = mod;
+				HMVertex nextVertex;
+				nextVertex.x = x;
+				nextVertex.z = z;
+				nextVertex.y = (*map[x])[z];
+				vertices[numVertex] = nextVertex;
+				numVertex++;
+			}
+
+			z = mod+1;
+			HMVertex nextVertex;
+			nextVertex.x = x;
+			nextVertex.z = z;
+			nextVertex.y = (*map[x])[z];
+			vertices[numVertex] = nextVertex;
+			numVertex++;
+			xPrev = x;
+			zPrev = z;
+		}
+	}
+}
+
 void HeightmapGenerator::fillMap(int iteration) {
 	int length = (map.size()-1) >> iteration;
 	int mapSize = map.size();
-	for (int xLow = 0; xLow < mapSize - 1; xLow += length) {
+	for(int xLow = 0; xLow < mapSize - 1; xLow += length) {
 		int xHigh = xLow + length;
-		for (int yLow = 0; yLow < mapSize - 1; yLow += length) {
+		for(int yLow = 0; yLow < mapSize - 1; yLow += length) {
 			int yHigh = yLow + length;
 			makeDiamond(iteration, xLow, xHigh, yLow, yHigh);
 		}
 	}
-	for (int xLow = ((length>>1)^-1)+1; xLow < mapSize - length; xLow += length) {
+	for(int xLow = ((length>>1)^-1)+1; xLow < mapSize - length; xLow += length) {
 		int xHigh = xLow + length;
-		for (int yLow = 0; yLow < mapSize - 1; yLow += length) {
+		for(int yLow = 0; yLow < mapSize - 1; yLow += length) {
 			int yHigh = yLow + length;
 			makeSquare(iteration, xLow, xHigh, yLow, yHigh);
 		}
 	}
-	for (int yLow = ((length>>1)^-1)+1; yLow < mapSize - length; yLow += length) {
+	for(int yLow = ((length>>1)^-1)+1; yLow < mapSize - length; yLow += length) {
 		int yHigh = yLow + length;
-		for (int xLow = 0; xLow < mapSize - 1; xLow += length) {
+		for(int xLow = 0; xLow < mapSize - 1; xLow += length) {
 			int xHigh = xLow + length;
 			makeSquare(iteration, xLow, xHigh, yLow, yHigh);
 		}
@@ -80,15 +139,15 @@ void HeightmapGenerator::makeSquare(int iteration, int xLow, int xHigh, int yLow
 	int random = randomNumber(iteration);
 	int newValue = ((val1 + val2 + val3 + val4) >> 2) + random;
 	(*map[x])[y] = newValue;
-	if (x == 0) {
+	if(x == 0) {
 		(*map[map.size()-1])[y] = newValue;
 	}
-	if (y == 0) {
+	if(y == 0) {
 		(*map[x])[map.size()-1] = newValue;
 	}
 }
 
-int HeightmapGenerator::randomNumber(int iteration){
+int HeightmapGenerator::randomNumber(int iteration) {
 	int random = (((rand()%variation)<<1)-variation)>>iteration;
 	return random;
 }
