a62c8ea68e820ae2740f0bd2f2a063dfad76b0f7
Michel Bouwmans <m.bouwmans@ep-games.eu>
2012-04-21 21:51:59 +0200

    Fixed colour format in fragment shader, colours now correct

--- shaders/Fragment-Colour Height.glsl
+++ shaders/Fragment-Colour Height.glsl
@@ -8,18 +8,18 @@ void main(void) {
 	
 	if(y < 0.0) {
 		//Water
-		colour = vec3(0., 0., 255.);
+		colour = vec3(0., 0., 255./255.);
 	} else if (y < 0.25) {
 		//Sand
-		colour = vec3(244., 240., 135.);
+		colour = vec3(244./255., 240./255., 135./255.);
 	} else if(y < 0.5) {
 		//Green
-		colour = vec3(0., 145., 0.);
+		colour = vec3(0., 145./255., 0.);
 	} else if(y < 0.75) {
-		colour = vec3(150., 106., 59.);
+		colour = vec3(150./255., 106./255., 59./255.);
 	} else {
 		//WIT, KOUD!!!! :(
-		colour = vec3(255., 255., 255.);
+		colour = vec3(255./255., 255./255., 255./255.);
 	}
 	
 	fragColour = vec4(colour, 1.);
