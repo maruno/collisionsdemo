e9689037c0791cc6f9ee1ba13b06f5eb162b635f
Michel Bouwmans <m.bouwmans@ep-games.eu>
2012-05-29 00:50:32 +0200

    VertexBufferFactory now uses new own obj modelloader

--- src/render/vertexbufferfactory.cpp
+++ src/render/vertexbufferfactory.cpp
@@ -1,6 +1,10 @@
 #include "render/vertexbufferfactory.hpp"
 
-#include "objectloader/objLoader.h"
+#include <fstream>
+#include <vector>
+
+#include "src/modelloader/objparser.yy.hpp"
+#include "modelloader/objlexer.hpp"
 
 using namespace render;
 
@@ -13,9 +17,14 @@ const std::tuple<const GLuint, const GLuint> VertexBufferFactory::operator[] (st
 	}
 	
 	//VBO not in flyweight pool: Load and buffer obj-file to VBO
-	objLoader loader;
+	std::ifstream inputFile("./models/" + objName + ".obj");
+	modelloader::ObjLexer lexer(&inputFile);
 	
-	loader.load(const_cast<char*>(std::string("./models/" + objName + ".obj").c_str()));
+	std::vector<float> verticesData;
+	std::vector<unsigned int> indicesData;
+	
+	modelloader::ObjParser parser(verticesData, indicesData, lexer);
+	parser.parse();
 	
 	//Allocate room for a VBO and IBO
 	GLuint bos[2];
@@ -25,21 +34,10 @@ const std::tuple<const GLuint, const GLuint> VertexBufferFactory::operator[] (st
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bos[1]);
 	
 	//Buffer vertices to VBO
-	glBufferData(GL_ARRAY_BUFFER, loader.vertexCount * sizeof(obj_vector), *loader.vertexList, GL_STATIC_DRAW);
-	
-	//Create vertex indices array
-	GLuint vertexindices[loader.faceCount * 3];
-	
-	for(int i = 0; i < loader.faceCount; i++) {
-		obj_face face = *(loader.faceList)[i];
-		
-		vertexindices[i*3] = face.vertex_index[0];
-		vertexindices[i*3+1] = face.vertex_index[1];
-		vertexindices[i*3+2] = face.vertex_index[2];
-	}
+	glBufferData(GL_ARRAY_BUFFER, verticesData.size() * sizeof(float), &(verticesData.front()), GL_STATIC_DRAW);
 	
 	//Buffer indices to IBO
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, loader.faceCount * 3 * sizeof(GLuint), vertexindices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesData.size() * sizeof(unsigned int), &(indicesData.front()), GL_STATIC_DRAW);
 	
 	std::tuple<GLuint, GLuint> vertexBuffer(bos[0], bos[1]);
 	
