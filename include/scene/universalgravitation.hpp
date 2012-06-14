7239f91efcec6538bc90af339ac02c5285f5f8cf
Michel Bouwmans <m.bouwmans@ep-games.eu>
2012-06-14 12:57:29 +0200

    Create class UniversalGravitation that computes the likenamed law from Sir Isaac Newton

---
+++ include/scene/universalgravitation.hpp
@@ -0,0 +1,30 @@
+#ifndef UNIVERSALGRAVITATION_HPP
+#define UNIVERSALGRAVITATION_HPP
+
+#include <vector>
+#include <algorithm>
+
+#include "scene/gravitationalobject.hpp"
+
+namespace scene {
+	class UniversalGravitation {
+		private:
+			static constexpr double G = 0.0000000000667384;
+			
+			std::vector<GravitationalObject*> gravityObjects;
+			
+			inline void addObject(GravitationalObject* gravObject);
+			inline void delObject(GravitationalObject* gravObject);
+			
+			void update();
+	};
+	
+	void UniversalGravitation::addObject(GravitationalObject* gravObject) {
+		gravityObjects.push_back(gravObject);
+	}
+
+	void UniversalGravitation::delObject(GravitationalObject* gravObject) {
+		std::remove(gravityObjects.begin(), gravityObjects.end(), gravObject);
+	}
+}
+#endif // UNIVERSALGRAVITATION_HPP
