24b67ef9b2a4c1eb95fb09c717418b0e6297bd8a
Marcel <Marcel_Veltman@hotmail.com>
2012-06-26 11:19:41 +0100

    more collision detection (Sphere&AABC)

--- include/scene/collisiondetection/CollisionDetector.hpp
+++ include/scene/collisiondetection/CollisionDetector.hpp
@@ -1,13 +1,14 @@
 /*
  * CollisionDetector.hpp
  *
- *      Author: Marcel Veltman
+ *      @author: Marcel Veltman
  */
 
 #ifndef COLLISIONDETECTOR_HPP_
 #define COLLISIONDETECTOR_HPP_
 
 #include <typeinfo>
+#include <glm/glm.hpp>
 
 #include "scene/collisiondetection/BoundingSphere.hpp"
 #include "scene/collisiondetection/BoundingVolume.hpp"
@@ -16,6 +17,9 @@ namespace scene {
 
 	namespace collisiondetection {
 
+		/**
+		 * Returns true when the bounding volumes collide.
+		 */
 		bool intersects(BoundingVolume& a, BoundingVolume& b) {
 			bool collision = false;
 			std::type_info typeA = typeid(a);
@@ -28,6 +32,14 @@ namespace scene {
 				AxisAlignedBoundingCuboid& aabca = static_cast<AxisAlignedBoundingCuboid&>(a);
 				AxisAlignedBoundingCuboid& aabcb = static_cast<AxisAlignedBoundingCuboid&>(b);
 				collision = _intersects(aabca, aabcb);
+			} else if(typeA == typeid(AxisAlignedBoundingCuboid) && typeB == typeid(BoundingSphere)) {
+				AxisAlignedBoundingCuboid& cuboid = static_cast<AxisAlignedBoundingCuboid&>(a);
+				BoundingSphere& sphere = static_cast<BoundingSphere&>(b);
+				collision = _intersects(cuboid, sphere);
+			} else if(typeA == typeid(BoundingSphere) && typeB == typeid(AxisAlignedBoundingCuboid)) {
+				BoundingSphere& sphere = static_cast<BoundingSphere&>(a);
+				AxisAlignedBoundingCuboid& cuboid = static_cast<AxisAlignedBoundingCuboid&>(b);
+				collision = _intersects(cuboid, sphere);
 			}
 			return collision;
 		}
@@ -55,6 +67,43 @@ namespace scene {
 			} else if(a.getMinZ() > b.getMaxZ() || a.getMaxZ() < b.getMinZ()) {
 				collision = false;
 			}
+
+			return collision;
+		}
+
+		bool _intersects(AxisAlignedBoundingCuboid& cuboid, BoundingSphere& sphere) {
+			bool collision = false;
+			glm::vec3 closestPoint;
+
+			if(sphere.getLocation().x < cuboid.getMinX()) {
+				closestPoint.x = cuboid.getMinX();
+			} else if(sphere.getLocation().x > cuboid.getMaxX()) {
+				closestPoint.x = cuboid.getMaxX();
+			} else {
+				closestPoint.x = sphere.getLocation().x;
+			}
+
+			if(sphere.getLocation().y < cuboid.getMinY()) {
+				closestPoint.y = cuboid.getMinY();
+			} else if(sphere.getLocation().y > cuboid.getMaxY()) {
+				closestPoint.y = cuboid.getMaxY();
+			} else {
+				closestPoint.y = sphere.getLocation().y;
+			}
+
+			if(sphere.getLocation().z < cuboid.getMinZ()) {
+				closestPoint.z = cuboid.getMinZ();
+			} else if(sphere.getLocation().z > cuboid.getMaxZ()) {
+				closestPoint.z = cuboid.getMaxZ();
+			} else {
+				closestPoint.z = sphere.getLocation().z;
+			}
+
+			float xSquare = closestPoint.x*closestPoint.x;
+			float ySquare = closestPoint.y*closestPoint.y;
+			float zSquare = closestPoint.z*closestPoint.z;
+			float distanceToCentre = std::sqrt(xSquare+ySquare+zSquare);
+			collision = distanceToCentre < sphere.getRadius();
 
 			return collision;
 		}
