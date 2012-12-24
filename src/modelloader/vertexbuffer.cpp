9ea91ff56a13aaa897c1e780c7f14362cbc3a226
Michel Bouwmans <m.bouwmans@ep-games.eu>
2012-12-24 13:48:26 +0100

    More sensible indentation of constructor with initialisation list

--- src/modelloader/vertexbuffer.cpp
+++ src/modelloader/vertexbuffer.cpp
@@ -3,18 +3,18 @@
 using namespace modelloader;
 
 VertexBuffer::VertexBuffer(GLuint myVBO, GLuint myIBO, GLuint myNBO, unsigned int myNumIndices)
-	: vBO(myVBO), iBO(myIBO), nBO(myNBO), numIndices(myNumIndices) {
-		//Generate VAO
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
+: vBO(myVBO), iBO(myIBO), nBO(myNBO), numIndices(myNumIndices) {
+	//Generate VAO
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-		glBindBuffer(GL_ARRAY_BUFFER, vBO);
-		glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-		glEnableVertexAttribArray(0);
-		
-		glBindBuffer(GL_ARRAY_BUFFER, nBO);
-		glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
-		glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, vBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, nBO);
+	glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
+	glEnableVertexAttribArray(1);
 }
 
 void VertexBuffer::bindBuffers() const {
