54f991186640cd1ac4d83d32b284b2d5ab3e10ef
Michel Bouwmans <m.bouwmans@ep-games.eu>
2013-01-02 17:32:36 +0100

    Add basic tunnel model

---
+++ src/tunnelsegment.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+#include <utility>
+
+#include <glm/glm.hpp>
+
+#include "renderer/render/colouredphongsceneitem.hpp"
+
+class TunnelSegment : public render::ColouredPhongSceneItem {
+ private:
+	const glm::vec3 difficultyVector;
+	std::vector<std::pair<glm::vec3, glm::mat4>> obstacles;
+
+ public:
+	TunnelSegment(glm::vec3& location, glm::vec3& difficulty);
+
+	inline const glm::vec3& getDifficultyVector();
+};
+
+const glm::vec3& TunnelSegment::getDifficultyVector() {
+	return difficultyVector;
+}
