54fd64250d6e67bcb6bee94f57d236bfcc03bbb9
Michel Bouwmans <m.bouwmans@ep-games.eu>
2013-01-05 14:18:54 +0100

    TunnelGenerator: Offset tweaks and fix obstacles-number

--- src/tunnelgenerator.cpp
+++ src/tunnelgenerator.cpp
@@ -12,19 +12,19 @@ TunnelGenerator& TunnelGenerator::operator++() {
 
 	if(current != nullptr) {
 		segmentLocation = current->getLocation();
-		segmentLocation += glm::vec3{0.0f, 0.0f, -0.1f};
+		segmentLocation += glm::vec3{0.0f, 0.0f, -0.4f};
 	}
 
 	//Calculate difficulty of tunnel segment
 	//TODO Add probabilities and use information from current
 	glm::vec3 difficultyVector;
 
-	boost::random::uniform_int_distribution<> positionDist(1,6);
-	difficultyVector.x = (positionDist(gen)-3)/10.0f;
-	difficultyVector.y = (positionDist(gen)-3)/10.0f;
+	boost::random::uniform_int_distribution<> positionDist(-2, 2);
+	difficultyVector.x = (positionDist(gen))/10.0f;
+	difficultyVector.y = (positionDist(gen))/10.0f;
 
 	boost::random::uniform_int_distribution<> obstaclesDist(1,4);
-	difficultyVector.z = obstaclesDist(gen)/10.0f;
+	difficultyVector.z = obstaclesDist(gen);
 
 	//Invalidates old iterator and creates new current
 	current.reset(new TunnelSegment{segmentLocation, difficultyVector});
